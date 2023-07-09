e60fdeb0f3b59f7e62e18fba459a8296bb5aaa16
Rafiul <hredoy.rafiul@gmail.com>
2023-07-09 09:29:35 +0600

    updated

--- js projects/fahrenheit conv/index.js
+++ js projects/fahrenheit conv/index.js
@@ -1,2 +1,3 @@
-var fahren=parseInt(prompt("give a random number:"));
-var celsius=(fahren-32)*(5/9);
+var fahren = parseInt(prompt("give a random number:"));
+var celsius = (fahren - 32) * (5 / 9);
+document.write(" temparature in celsius scale :" + celsius);
