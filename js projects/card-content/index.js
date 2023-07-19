08f52c4ae37414b5c02ae7d3a5b74af88afd85f8
Rafiul <hredoy.rafiul@gmail.com>
2023-07-19 13:20:15 +0600

    async added for axios

--- js projects/card-content/index.js
+++ js projects/card-content/index.js
@@ -1,66 +1,33 @@
-const posts = [
-  {
-    title: "this is title 1",
-    body: "this is body1",
-  },
+const fecthData = async (config) => {
+  try {
+    const res = await axios(config);
+    return res.data;
+  } catch (error) {
+    throw Error("data is not fetched");
+  }
+};
 
-  {
-    title: "this is title 2",
-    body: "this is body 2",
-  },
-
-  {
-    title: "this is title 3",
-    body: "this is body 4",
-  },
-
-  {
-    title: "this is title 4",
-    body: "this is body 4",
-  },
-  {
-    title: "this is title 5",
-    body: "this is body5",
-  },
-
-  {
-    title: "this is title 6",
-    body: "this is body 6",
-  },
-
-  {
-    title: "this is title 7",
-    body: "this is body 7",
-  },
-
-  {
-    title: "this is title 8",
-    body: "this is body 8",
-  },
-
-  /* <div class="post">
-<h4 class="post-title">post title</h4>
-<p class="post-body">post description 1</p>
-</div> */
-
-  //selection
-];
+// selection
 const postsElement = document.querySelector(".posts");
-const loadAllData = () => {
-  
-  
-  // Below in each iteration every element from posts array is inserted to post variable
-  //
-    posts.map((post) => {
-    // the following statement is telling a div is going to be created
-    const postElement = document.createElement("div");
-    // the following statement sets what kind of div will be created(through mentioning classname)
-    postElement.classList.add("post");
-    // this following statement tells what would be the contents
-    postElement.innerHTML = `<h4 class="post-title">post title</h4>
-    <p class="post-body">post description 1</p>`;
 
+const loadAllData = async () => {
+  const posts = await fecthData("https://jsonplaceholder.typicode.com/posts");
+
+  // Below in each iteration every element from posts array is inserted to post variable
+  posts.map((post) => {
+    // this following statement tells what would be the contents
+    const postElement = document.createElement("div");
+
+    // the following statement is telling a div is going to be created
+    postElement.classList.add("post");
+    // the following statement sets what kind of div will be created(through mentioning classname)
+
+    postElement.innerHTML = `
+           <h4 class="post-title">${post.title}</h4>
+          <p class="post-body">${post.body}</p>
+      `;
     postsElement.appendChild(postElement);
   });
 };
+
 loadAllData();
